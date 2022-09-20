af38802cc213b34aae04ede805ffcb0e2e769eac
sumanveera0412 <107625169+sumanveera0412@users.noreply.github.com>
2022-09-20 21:39:45 +0530

    Add files via upload

---
+++ myportal-client/src/test/java/com/springcorsclient/myportalclient/MyportalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcorsclient.myportalclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportalClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
